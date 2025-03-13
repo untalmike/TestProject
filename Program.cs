03f3eb5a9fa6e2d27a6ec395d9ea8b47d1681237
untalmike <miguel.gonzalezl@hotmail.com>
2025-03-13 00:42:03 -0600

    Proyecto final

--- Program.cs
+++ Program.cs
@@ -1,45 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Random dice = new Random();
-
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+﻿Random random = new Random();
+int dayUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
 
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// Your code goes here
+if(dayUntilExpiration <= 0)
 {
-    if((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples! +6 bonus to total!");
-        total += 6;
-    }
-    else 
-    {
-        Console.WriteLine("You rolled doubles! +2 bonus to total!");
-        total+=2;    
-    }
-
-    Console.WriteLine($"Your total including the bonus: {total}");
+    Console.WriteLine("Your subscription has expired.");
+}
+else if(dayUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expíres within a day!");
+    discountPercentage = 20;
+}
+else if(dayUntilExpiration <= 5){
+    Console.WriteLine($"Your subscription expires in {dayUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if(dayUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
 }
 
-
-if(total >= 16)
+if(discountPercentage > 0)
 {
-    Console.WriteLine("You win a new car!");
-}
-else if(total >= 10)
-{
-    Console.WriteLine("You win a new laptop!");
-}
-else if(total == 7)
-{
-    Console.WriteLine("You win a trip for two!");
-}
-else
-{
-    Console.WriteLine("You win a kitten!");
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
 }
