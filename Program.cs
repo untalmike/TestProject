6da9a8873fabc3931bd8726f2f45b56f92bfe582
untalmike <miguel.gonzalezl@hotmail.com>
2025-03-21 00:42:03 -0600

    Primeros pasos en matrices

--- Program.cs
+++ Program.cs
@@ -1,28 +1,15 @@
-﻿Random random = new Random();
-int dayUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+﻿string[] fraudulentOrderIDs = new string[3];
 
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[3] = "D000";
 
-// Your code goes here
-if(dayUntilExpiration <= 0)
-{
-    Console.WriteLine("Your subscription has expired.");
-}
-else if(dayUntilExpiration == 1)
-{
-    Console.WriteLine("Your subscription expíres within a day!");
-    discountPercentage = 20;
-}
-else if(dayUntilExpiration <= 5){
-    Console.WriteLine($"Your subscription expires in {dayUntilExpiration} days.");
-    discountPercentage = 10;
-}
-else if(dayUntilExpiration <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
 
-if(discountPercentage > 0)
-{
-    Console.WriteLine($"Renew now and save {discountPercentage}%.");
-}
+// Reassigning the first element
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
--- obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0db06535c0295640295c224052521ecc6de59ae1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f3eb5a9fa6e2d27a6ec395d9ea8b47d1681237")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
