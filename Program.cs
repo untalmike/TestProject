e05999adad16c87b723defe9794baa5628d26099
untalmike <miguel.gonzalezl@hotmail.com>
2025-03-21 00:44:26 -0600

    Agrupación de distinta forma en matrices

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
-﻿string[] fraudulentOrderIDs = new string[3];
+﻿// string[] fraudulentOrderIDs = new string[3];
 
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+
+string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
+// second format
+// string[] fraudulentOrderIDs = {"A123", "B456", "C789"};
 // fraudulentOrderIDs[3] = "D000";
 
 Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
