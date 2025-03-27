9532bc0b608bbfdb941d582b5ff36cc809a924a7
untalmike <miguel.gonzalezl@hotmail.com>
2025-03-27 00:37:35 -0600

    Pedidos fraudulentos, prueba de aprendizaje

--- obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ab2452de1bc5f8c12cdb6b43861607148655b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d91a871d51d909ef4601fdf6b818cb61fee4c24")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
