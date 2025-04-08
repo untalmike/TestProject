2fd6ef405c9cbc21b00ca44c40f07f021ddf4a53
untalmike <miguel.gonzalezl@hotmail.com>
2025-04-08 02:09:29 -0600

    agrupación de alumnos, arreglo para scores, grades y sumas generales, integración de alumnos faltantes

--- Program.cs
+++ Program.cs
@@ -20,42 +20,45 @@ using System;
 
 // initialize variables - graded assignments 
 int currentAssignments = 5;
+
 int[] sophiaScores = new int[5] {90, 86, 87, 98, 100};
 int[] andrewScores = new int[5] {92, 89, 81, 96, 90};
 int[] emmaScores = new int[5] {90, 85, 87, 98, 68};
 int[] loganScores = new int[5] {90, 95, 87, 88, 96};
 
-
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
-
 string[] studentNames = new string[] {"Sophia", "Andrew", "Emma", "Logan"};
+
+int[] studentScores = new int[10];
+
+Console.WriteLine("Student\t\tGrade\n");
 
 foreach (string name in studentNames)
 {
-    if (name == "Sophia")
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+    else if(currentStudent == "Andrew")
+        studentScores = andrewScores;
+    else if(currentStudent == "Emma")
+        studentScores = emmaScores;
+    else if(currentStudent == "Logan")
+        studentScores = loganScores;
+
+    int sumAssignmentScores = 0;
+    
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
     {
-        int sophiaSum = 0;
-        decimal sophiaScore;
-
-        foreach (int score in sophiaScores)
-        {
-            sophiaSum += score;
-        }
-        
-        sophiaScore = (decimal)sophiaSum / currentAssignments;
-
-        Console.WriteLine("Student\t\tGrade\n");
-        Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+        sumAssignmentScores += score;
     }
+    
+    currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
 
+    Console.WriteLine($"{currentStudent}:\t\t{currentStudentGrade}\t?");
 
 }
-
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
