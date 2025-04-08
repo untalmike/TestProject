c93427b5c3d644b5ae6babece7c26f2e2dfaac1d
untalmike <miguel.gonzalezl@hotmail.com>
2025-04-08 01:12:16 -0600

    Prueba de comentarios con multilineas

--- obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9532bc0b608bbfdb941d582b5ff36cc809a924a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d40bcf94bd362a400549e457afaf53e06d35feac")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
