d40bcf94bd362a400549e457afaf53e06d35feac
untalmike <miguel.gonzalezl@hotmail.com>
2025-04-08 01:08:47 -0600

    Prueba de comentarios

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
-﻿string firstName = "Bob";
-int widgetsPurchased = 7;
+﻿/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
 
-// Testing a change to the message.
-// Int widgetSold = 7;
-// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
-Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
