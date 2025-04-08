e125493c58eb9a7849f3072affa1372705160ceb
untalmike <miguel.gonzalezl@hotmail.com>
2025-04-08 01:20:56 -0600

    ejercicio mejorado por propuesta

--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
-﻿string str = "The quick brown fox jumps over the lazy dog.";
+﻿/*
+    This code reverses a message, counts the number of times
+    a particular character appears, then prints the results
+    to console window.
+*/
+string str = "The quick brown fox jumps over the lazy dog.";
 
-// convert the message into a char array
 char[] charMessage = str.ToCharArray();
 
-// Reverse the chars
 Array.Reverse(charMessage);
 int x = 0;
 
-// count the o's
 foreach (char i in charMessage) { 
     if (i == 'o') { 
         x++; 
     } 
 }
-// convert it back to a string
 string new_message = new String(charMessage);
 
-// print it out
 Console.WriteLine(new_message);
 Console.WriteLine($"'o' appears {x} times.");
